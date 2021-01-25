550da389e914b9e21ee05e7b4ebfc8ffa59f0507
sa1583 <sa1583@naver.com>
2021-01-25 13:48:07 +0900

    RFT: 코드 스타일 개선

--- app/src/main/java/com/example/recipekt/AppDatabase.kt
+++ app/src/main/java/com/example/recipekt/AppDatabase.kt
@@ -6,24 +6,24 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [DBRecipe::class, DBGredient::class], version = 1, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun RecipeDAO(): RecipeDAO
 
     companion object {
         private var instance: AppDatabase? = null
 
         @Synchronized
-        fun getInstance(context: Context): AppDatabase? {
-            if(instance == null) {
+        fun getInstance(context: Context): AppDatabase {
+            if (instance == null) {
                 instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "database-recipe"
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "database-recipe"
                 )
-                .allowMainThreadQueries()
-                .build()
+                        .allowMainThreadQueries()
+                        .build()
             }
-            return instance
+            return instance!!
         }
     }
 }
