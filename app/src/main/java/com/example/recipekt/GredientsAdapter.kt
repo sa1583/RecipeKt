ebb82275cedc8d17b157d411de97dc8290e53fe1
sa1583 <sa1583@naver.com>
2021-01-25 14:02:09 +0900

    ADD: 재료목록 리사이클러뷰 업데이트 방식 추가      재료 목록을 recyclerAdapter에서 관리하게 변경

--- app/src/main/java/com/example/recipekt/GredientsAdapter.kt
+++ app/src/main/java/com/example/recipekt/GredientsAdapter.kt
@@ -2,23 +2,35 @@ package com.example.recipekt
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipekt.databinding.ListGredientsBinding
 
-class GredientsAdapter(val gredientList: ArrayList<Gredient>): RecyclerView.Adapter<GredientsAdapter.CustomViewHolder>() {
+class GredientsAdapter : RecyclerView.Adapter<GredientsAdapter.CustomViewHolder>() {
+
+    private val gredientList = mutableListOf<Gredient>()
+
+    val gredients: List<Gredient>
+        get() = gredientList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val binding = ListGredientsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CustomViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return gredientList.size
-    }
+    override fun getItemCount() = gredientList.size
+
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val gredient: Gredient = gredientList[position]
         holder.bind(gredient)
+    }
+
+    fun update(newGredients: List<Gredient>) {
+        val diffResult = DiffUtil.calculateDiff(ContentDiffUtil(gredientList, newGredients), false)
+        diffResult.dispatchUpdatesTo(this)
+        gredientList.clear()
+        gredientList.addAll(newGredients)
     }
 
     class CustomViewHolder(val binding: ListGredientsBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -28,6 +40,20 @@ class GredientsAdapter(val gredientList: ArrayList<Gredient>): RecyclerView.Adap
             binding.tvUnit.text = gredient.Unit
         }
     }
+}
+
+class ContentDiffUtil(private val oldList: List<Gredient>, private val curList: List<Gredient>) : DiffUtil.Callback() {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].name == curList[newItemPosition].name
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == curList[newItemPosition]
+    }
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = curList.size
 
 
 }
